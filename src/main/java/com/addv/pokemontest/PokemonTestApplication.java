fe413e69ac1fcb557e9c49a3e06374836fdee228
Erick Garcia <erick.isc.dante@gmail.com>
2023-11-23 15:16:12 -0600

    first commit

---
+++ src/main/java/com/addv/pokemontest/PokemonTestApplication.java
@@ -0,0 +1,13 @@
+package com.addv.pokemontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/addv/pokemontest/PokemonTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.addv.pokemontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
